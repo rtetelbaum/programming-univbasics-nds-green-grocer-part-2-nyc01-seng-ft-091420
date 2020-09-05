cc4cb0c0379846022c44cb0cdcfeb746d0df0e5a
Roman Tetelbaum <rtetelbaum@mailbox.org>
2020-09-05 05:13:30 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -23,7 +23,8 @@ def apply_clearance(cart)
   clearance_cart = []
   cart.each do |item|
     if item[:clearance] == true
-      clearance_price = (item[:price] *= 0.8)
+      clearance_price = item[:price] *= 0.8
+      item[:price] = clearance_price.round(2)
       clearance_cart << item
     else
       clearance_cart << item
